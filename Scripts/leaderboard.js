39d0472190b59b419ccefcbaec8e93fb549251a7
Tai Yu Lin <155038045+tia-tai@users.noreply.github.com>
2025-04-16 14:36:13 -0400

    Update leaderboard.js

--- Scripts/leaderboard.js
+++ Scripts/leaderboard.js
@@ -15,7 +15,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: secrets.FIRESTORE_API_KEY,
+  apiKey: "AIzaSyBKJT-A3KHH_hoJV-6-84mO3vJ3Rh4wzW4",
   authDomain: "iste-d7d9f.firebaseapp.com",
   projectId: "iste-d7d9f",
   storageBucket: "iste-d7d9f.firebasestorage.app",
