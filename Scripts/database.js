662f357d00027b5bc0ceedf8f1ddd5a2fb3a5907
Tiff Zhang <tzhang051308@gmail.com>
2025-10-26 17:18:34 -0400

    Update database.js

--- Scripts/database.js
+++ Scripts/database.js
@@ -16,7 +16,7 @@ import {
 import { GoogleGenerativeAI } from "https://cdn.jsdelivr.net/npm/@google/generative-ai/+esm";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8d-9lOYZ0GUZJdcSq5VmAjxl2F8w7PJQ",
+  apiKey: "AIzaSyBKJT-A3KHH_hoJV-6-84mO3vJ3Rh4wzW4",
   authDomain: "iste-d7d9f.firebaseapp.com",
   projectId: "iste-d7d9f",
   storageBucket: "iste-d7d9f.firebasestorage.app",
